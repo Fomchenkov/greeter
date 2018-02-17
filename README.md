f31a6b59a4d1895dc1148c711c78023df9435fd7
Your Name <you@example.com>
2018-02-17 21:53:36 +0300

    Added new function

---
+++ README.md
@@ -0,0 +1,21 @@
+# Must Have Greeter library
+
+# Installing
+
+`$ go get github.com/Fomchenkov/greeter`
+
+# Usage
+
+```go
+package main 
+
+import (
+	"fmt"
+	"github.com/Fomchenkov/greeter"
+)
+
+func main() {
+	fmt.Println(greeter.Greet("Fomchenkov"))
+}
+
+```
--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package greeter
 
-// Greet user
+// Greet func
 func Greet(name string) string {
 	return "Hello " + name + "!"
+}
+
+// GreetAny func
+func GreetAny(names []string) string {
+	res := ""
+	for i := 0; i < len(names); i++ {
+		res += "Hello " + names[i] + "!\n"
+	}
+	return res[0:len(res)-1]
 }
